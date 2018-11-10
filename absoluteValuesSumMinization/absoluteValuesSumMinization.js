59d18fc88b4f27e3c4cbc5d08e99878b4133bcec
Christopher Beards <BeardsCM@gmail.com>
2018-11-10 11:16:53 -0500

    absoluteValueSumMin; passing

--- absoluteValuesSumMinization/absoluteValuesSumMinization.js
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.js
@@ -1,3 +1,14 @@
-const absoluteValuesSumMinization = () => {};
+const absoluteValuesSumMinization = numbers => {
+  let absoluteSum;
+  let reduced;
+  // Add each number in the array
+  reduced = numbers.reduce((accu, num) => accu + num);
+
+  // Find median
+  absoluteSum = reduced / numbers.length;
+
+  // Return the largest integer less than or equal to the given number
+  return Math.floor(absoluteSum);
+};
 
 module.exports = absoluteValuesSumMinization;
