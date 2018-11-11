745a275b474a81573d481e19e5df3d3f506daa70
Christopher Beards <BeardsCM@gmail.com>
2018-11-11 14:04:19 -0500

    addBorder; added test

--- test/addBorder.test.js
+++ test/addBorder.test.js
@@ -2,6 +2,10 @@ const expect = require('chai').expect;
 const addBorder = require('../addBorder/addBorder.js');
 
 describe('Add Border', () => {
+  it('Should return an array', () => {
+    expect(addBorder(['abc', 'ded'])).to.be.an('array');
+  });
+
   it('Should return an array properly framed', () => {
     // expect().to.eql checks for deep equality or sameness
     expect(addBorder(['abc', 'ded'])).to.eql([
