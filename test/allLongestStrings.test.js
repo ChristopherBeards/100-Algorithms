eea8fddd46629d32a335e6dfe0dd73612019bf7d
Christopher Beards <BeardsCM@gmail.com>
2018-11-13 12:08:42 -0500

    allLongestStrings; deep equality check

--- test/allLongestStrings.test.js
+++ test/allLongestStrings.test.js
@@ -2,8 +2,14 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const allLongestStrings = require('../allLongestStrings/allLongestStrings.js');
 
+const testArray = ['aba', 'aa', 'ad', 'vcd', 'aba'];
+
 describe('All Longest Strings', () => {
   it('Should return an array', () => {
-    expect(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"])).to.be.an('array');
+    expect(allLongestStrings(testArray)).to.be.an('array');
   });
-})
+
+  it('Should return all the longest strings', () => {
+    expect(allLongestStrings(testArray)).to.be.eql(['aba', 'vcd', 'aba']);
+  })
+});
