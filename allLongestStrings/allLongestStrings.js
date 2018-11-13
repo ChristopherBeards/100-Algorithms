80c51885811a0851014d9d5bc7d7f159451d8e11
Christopher Beards <BeardsCM@gmail.com>
2018-11-13 12:19:04 -0500

    allLongestStrings; done

--- allLongestStrings/allLongestStrings.js
+++ allLongestStrings/allLongestStrings.js
@@ -1,5 +1,20 @@
 const allLongestStrings = (arr) => {
-  return arr;
+  // Initialize a counter variable
+  let count = 0;
+
+  // Initialize a storage array
+  let longestStrings = [];
+  
+  // Iterate over the given arr and compare values
+  arr.forEach(value => {
+    // Determine the value of the counter var
+    if (value.length > count) count = value.length;
+    // if the value length is equal to count add it to 
+    // the longestStrings array
+    if (value.length === count) longestStrings.push(value);
+  });
+
+  return longestStrings;
 };
 
 module.exports = allLongestStrings;
