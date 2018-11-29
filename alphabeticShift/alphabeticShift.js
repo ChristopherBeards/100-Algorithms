56aab5b398987599ee49d45ccd8055b25e037c8d
Christopher Beards <BeardsCM@gmail.com>
2018-11-28 23:36:20 -0500

    alphabeticShift; tests passing

--- alphabeticShift/alphabeticShift.js
+++ alphabeticShift/alphabeticShift.js
@@ -1,5 +1,40 @@
-const alphabeticShift = (str) => {
+const alphabeticShift = str => {
+  return (str = assessLetter(str));
+};
 
-}
+const assessLetter = str => {
+  const shifted = {
+    a: 'b',
+    b: 'c',
+    c: 'd',
+    d: 'e',
+    f: 'g',
+    g: 'h',
+    h: 'i',
+    i: 'j',
+    j: 'k',
+    k: 'l',
+    l: 'm',
+    m: 'n',
+    o: 'p',
+    p: 'q',
+    q: 'r',
+    r: 's',
+    s: 't',
+    t: 'u',
+    u: 'v',
+    v: 'w',
+    w: 'x',
+    x: 'y',
+    y: 'z',
+    z: 'a',
+  };
+
+  const shiftedString = str.split('').map(letter => {
+    return (letter = shifted[letter]);
+  });
+
+  return shiftedString.join('');
+};
 
 module.exports = alphabeticShift;
