c54855d0ea3c9a4284096579e83bd193fce3a994
Christopher Beards <BeardsCM@gmail.com>
2018-12-04 13:59:43 -0500

    alternatingSums; passing tests

--- alternatingSums/alternatingSums.js
+++ alternatingSums/alternatingSums.js
@@ -1,5 +1,13 @@
 const alternatingSums = (arr) => {
+  let teamSums = [0, 0];
 
+  arr.forEach((each, i) => {
+    i % 2 === 0 ? teamSums[0] += each : teamSums[1] += each; 
+  });
+
+  return teamSums;
 }
+
+console.log(alternatingSums([50, 60, 60, 45, 70]));
 
 module.exports = alternatingSums;
--- test/alternatingSums.test.js
+++ test/alternatingSums.test.js
@@ -1,2 +1,13 @@
 const alternatingSums = require('../alternatingSums/alternatingSums');
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+describe('Alternating Sums', () => {
+  it('Should return an array of values', () => {
+    expect(alternatingSums([50, 60, 60, 45, 70])).to.be.an('array');
+  });
+
+  it('Should return an array of two values; correctly totaling the values', () => {
+    assert.deepEqual(alternatingSums([50, 60, 60, 45, 70]), [180, 105]);
+  });
+});
